55db0b7661415d68010c34a2d63b3df7c8d586f8
SamuelNatanael <natanael.s2008@gmail.com>
2025-04-03 14:57:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -125,6 +125,18 @@ border-radius: 8px; will-change: transform;">
 </div>
 <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGHM0QbqQA&#x2F;Lj4De3jRwjXTOEt7T7QeIA&#x2F;view?utm_content=DAGHM0QbqQA&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Abrir en canva</a>`,
     },
+                {
+                       id: 9,
+        nombre: "Albert Einstein",
+        fecha: "03/04/25",
+        desc: "Desarrollo y bienestar del personal",
+        docente: "Patricia Avendaño",
+        tarea: `<div
+  <iframe    src="https://www.canva.com/design/DAGjaeWKR1E/9iza_w208NYvrFqov2LDHA/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGjaeWKR1E&#x2F;9iza_w208NYvrFqov2LDHA&#x2F;view?utm_content=DAGjaeWKR1E&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Abrir en canva</a>`, 
+                },
 
 
 ];
