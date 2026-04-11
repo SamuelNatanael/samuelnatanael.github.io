9e8738f04a30391c9dd71e2c8dc5e4ed082dbc6f
SamuelNatanael <natanael.s2008@gmail.com>
2026-04-11 13:50:05 -0400

    Actualizar jsmiper.js

--- jsmiper.js
+++ jsmiper.js
@@ -1,1 +1,40 @@
-const miper
+const tareas = [{
+
+        id: 1,
+        nombre: "Agroindustrial los campos S.A",
+        fecha: "2026",
+        desc: "Practicas seguras en las operaciones logísticas",
+        docente: "Magali Galleguillos",
+        tarea: `<iframe>
+       src="
+     </iframe>
+   `,
+    },
+}
+
+];
+
+tareas.forEach((prod) => {
+    const { id, nombre, fecha, desc, tarea, docente } = prod;
+    if (contenedor) {
+        contenedor.innerHTML += `<div class contenido>
+        <div class="card mt-3 item">
+        <div id="content">
+        <div id="${id}">
+        <div class="box">
+                <div id="${tarea}</div>
+                </div>
+            </div>
+
+            </div>
+        <div class="card-body">
+        <h3 class="card-title">${nombre}</h3>
+        <h4 class="card-text">Fecha: ${fecha}</h4>
+        <h4 class="card-text">${desc}</h4>
+        <h4 class="card-text">${docente}</h4>
+      </div>
+    </div>
+    </div>
+                `;
+    }
+}); 
