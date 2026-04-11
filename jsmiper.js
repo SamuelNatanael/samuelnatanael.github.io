1fe36160b0a0c4858705612cfae3e50bf9f689c2
SamuelNatanael <natanael.s2008@gmail.com>
2026-04-11 14:13:24 -0400

    Simplify tarea field in jsmiper.js
    Removed unnecessary iframe from tarea field.

--- jsmiper.js
+++ jsmiper.js
@@ -5,10 +5,7 @@ const tareas = [{
         fecha: "2026",
         desc: "Practicas seguras en las operaciones logísticas",
         docente: "Magali Galleguillos",
-        tarea: `<img id="miper" src="miper.png><iframe>
-       src="
-     </iframe>
-   `,
+        tarea: `<img id="miper" src="miper.png>`,
     },
 }
 
